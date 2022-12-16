24bda7d0931a1e3a965fc176999e91a30917507f
Olvenn <olga-vencel@yandex.ru>
2022-12-16 13:05:19 +0300

    Change privite-route

--- frontend/src/components/private-route/private-route.jsx
+++ frontend/src/components/private-route/private-route.jsx
@@ -1,12 +1,20 @@
 import { useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+import authContext from '../contexts/index.jsx';
 
 const PrivateRoute = ({ children }) => {
   const navigate = useNavigate();
+  const { theme } = useContext(authContext);
+  const className = theme.class;
   // const { logIn } = useContext(AuthContext);
-  const hasAccess = true;
+  const auth = useAuth();
   // alert(logIn);
 
   return hasAccess ? children : navigate('/login');
 };
 
 export default PrivateRoute;
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  return auth ? children : <Navigate to="/login" />;
+};
